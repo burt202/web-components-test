4747dd02456df5823abb3a1e4fc892186dcd7890
Aaron Burtnyk <aaron.burtnyk@britishgas.co.uk>
2022-12-22 20:45:41 +0000

    Add react

--- src/js/web-components/types.d.ts
+++ src/js/web-components/types.d.ts
@@ -1,6 +1,7 @@
-import {HelloWorld} from "./web-components/hello-world"
-import {WordCount} from "./web-components/word-count"
+import {HelloWorld} from "./hello-world"
+import {WordCount} from "./word-count"
 
+/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
 type CustomElement<T> = Partial<T & React.DOMAttributes<T> & {children: any}>
 
 declare global {
